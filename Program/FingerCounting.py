49e954d408889484abc2a5ba073fd70cc0e558e9
Biuuu2309 <minhvu7a1@gmail.com>
2024-10-25 22:03:54 +0700

    Update FingerCounting.py

--- Program/FingerCounting.py
+++ Program/FingerCounting.py
@@ -43,24 +43,50 @@ while True:
 
     if len(lmList) != 0:
         finger = []
-        if lmList[tipIds[0]][1] > lmList[tipIds[1]][1]: 
-            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:  
-                finger.append(1)  
+        #Right
+        if lmList[tipIds[0] - 3][1] > lmList[tipIds[0] - 4][1]:
+            #>90
+            
+            #0-90
+            if lmList[tipIds[4] - 3][1] > lmList[tipIds[0] - 4][1]:
+                #
+                if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:  
+                    if lmList[tipIds[1] - 3][1] > lmList[tipIds[0]][1]:
+                        finger.append(0)
+                    else:
+                        finger.append(1)  
+                else:
+                    finger.append(0)  
+                folderpath = "Python Project/Hand-Tracking/Image right"
+                for id in range(1, 5):
+                    if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                        finger.append(1)
+                    else:
+                        finger.append(0)
+                #
             else:
-                finger.append(0)  
-            folderpath = "Python Project/Hand-Tracking/Image right"
+                if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:  
+                    finger.append(1)  
+                else:
+                    finger.append(0)  
+                folderpath = "Python Project/Hand-Tracking/Image right"
+                for id in range(1, 5):
+                    if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                        finger.append(1)
+                    else:
+                        finger.append(0)
+        #Left
         else:
-           
             if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:  
                 finger.append(1)  
             else:
                 finger.append(0)  
             folderpath = "Python Project/Hand-Tracking/Image left"
-        for id in range(1, 5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                finger.append(1)
-            else:
-                finger.append(0)
+            for id in range(1, 5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    finger.append(1)
+                else:
+                    finger.append(0)
                 
         print(finger)
         mySerial.sendData(finger)
